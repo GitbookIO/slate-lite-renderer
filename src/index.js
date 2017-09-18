4d4bf773ce1de40d857c970e9201fa75777b3e33
Samy Pessé <samypesse@gmail.com>
2017-09-18 16:19:48 +0200

    Add rendering based on slate

--- src/index.js
+++ src/index.js
@@ -13,50 +13,158 @@ class LightRender extends React.PureComponent {
         // style: object
     };
 
-    static renderNode({ editor, state, schema, node }): React.Element<*> {
-        let children = null;
-        if (node.nodes) {
-            children = node.nodes.map(n =>
-                LightRender.renderNode({ editor, state, schema, node: n })
-            );
-        }
+    constructor(props: *) {
+        super(props);
 
-        // Get component
-        if (node.kind === 'block') {
-            const X =
-                schema.nodes[node.type] ||
-                (node.kind == 'block'
-                    ? props => <div {...props} />
-                    : props => <span {...props} />);
-
-            return (
-                <X
-                    key={node.key}
-                    editor={{ props: editor }}
-                    state={state}
-                    schema={schema}
-                    node={node}
-                >
-                    {children}
-                </X>
-            );
-        }
-        // else (text, inline, void)
-        return (
-            <span key={node.key} node={node}>
-                {node.text}
-            </span>
-        );
+        this.schema = Slate.Schema.create(props.schema || {});
     }
 
+    /*
+     * Render a mark.
+     */
+    renderMark = ({
+        mark,
+        marks,
+        node,
+        offset,
+        children
+    }: {
+        mark: Slate.Mark,
+        node: Slate.Text,
+        marks: *,
+        offset: number,
+        children: React.Element<*>
+    }): React.Element<*> => {
+        const { state } = this.props;
+
+        const Component = mark.getComponent(this.schema);
+        if (!Component) {
+            return children;
+        }
+
+        return (
+            <Component
+                editor={this}
+                mark={mark}
+                marks={marks}
+                node={node}
+                offset={offset}
+                schema={this.schema}
+                state={state}
+                text={node}
+            >
+                {children}
+            </Component>
+        );
+    };
+
+    /*
+     * Render a range of text.
+     */
+    renderRange = ({
+        range,
+        node,
+        offset
+    }: {
+        range: Slate.Range,
+        node: Slate.Text,
+        offset: number
+    }): React.Element<*> => {
+        const { marks } = range;
+
+        return marks.reduce(
+            (children, mark) =>
+                this.renderMark({
+                    mark,
+                    marks,
+                    node,
+                    children,
+                    offset
+                }),
+            range.text
+        );
+    };
+
+    /*
+     * Render a text node.
+     */
+    renderText = ({ node }: { node: Slate.Text }): React.Element<*> => {
+        const { document } = this.props.state;
+        const decorators = this.schema.hasDecorators
+            ? document.getDescendantDecorators(node.key, this.schema)
+            : [];
+
+        const ranges = node.getRanges(decorators);
+        let offset = 0;
+
+        const leaves = ranges.map((range, i) => {
+            const leaf = this.renderRange({
+                range,
+                node,
+                offset
+            });
+            offset += range.text.length;
+            return leaf;
+        });
+
+        return <span data-key={node.key}>{leaves}</span>;
+    };
+
+    /*
+     * Render a node.
+     */
+    renderNode = ({
+        node,
+        parent
+    }: {
+        node: Slate.Document | Slate.Inline | Slate.Block,
+        parent: Slate.Document | Slate.Inline | Slate.Block
+    }): React.Element<*> => {
+        const Component = node.getComponent(this.schema);
+
+        const attributes = {
+            'data-key': node.key
+        };
+
+        // If it's a block node with inline children, add the proper `dir` attribute
+        // for text direction.
+        if (node.kind == 'block' && node.nodes.first().kind != 'block') {
+            const direction = node.getTextDirection();
+            if (direction == 'rtl') attributes.dir = 'rtl';
+        }
+
+        return (
+            <Component
+                attributes={attributes}
+                editor={this}
+                isSelected={false}
+                key={node.key}
+                node={node}
+                parent={parent}
+                readOnly
+                state={this.props.state}
+            >
+                {node.isVoid ? null : (
+                    node.nodes.map(child => {
+                        if (child.kind == 'text') {
+                            return this.renderText(child);
+                        }
+                        return this.renderNode({ node: child, parent: node });
+                    })
+                )}
+            </Component>
+        );
+    };
+
     render() {
-        const { state, schema, editor } = this.props;
+        const { state } = this.props;
+        const { document } = state;
 
         return (
             <div className="editor">
-                <div data-slate-editor="true" key={state.document.key}>
-                    {state.document.nodes.map(node =>
-                        LightRender.renderNode({ editor, state, schema, node })
+                <div data-slate-editor="true">
+                    {document.nodes.map(node =>
+                        this.renderNode({ node, parent: document })
                     )}
                 </div>
             </div>
